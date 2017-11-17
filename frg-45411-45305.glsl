192ed5d5f37d63a0ad6332e65abfd3d6aaa983ae
awman <31491994+awman@users.noreply.github.com>
2017-11-17 14:57:51 +0000

    Create frg-45411-45305.glsl

---
+++ frg-45411-45305.glsl
@@ -0,0 +1,10 @@
+precision mediump float;
+varying vec4 fPosition;
+varying vec4 fColor;
+
+void main() {
+    //gl_FragColor = fColor;
+    gl_FragColor = fPosition;
+    //gl_FragColor = vec4(0,0,1,1);
+    
+}
